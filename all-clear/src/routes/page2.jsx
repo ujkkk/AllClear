eaa631f670e3cfba1e619b8837b9c478a997ca19
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-10-31 12:45:44 +0900

    file remove

--- all-clear/src/routes/page2.jsx
+++
@@ -1,7 +0,0 @@
-export default function Page2() {
-  return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Page2</h2>
-    </main>
-  );
-}
--- all-clear/src/routes/standBy.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import StanByScreen from "../components/game/standByScreen";
-
-export default function StanBy() {
-    const GAME_TITLE = "스탠바이"
-    return (
-        <StanByScreen title={GAME_TITLE} />
-    );
-}
