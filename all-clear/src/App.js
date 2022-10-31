fc4ff30cb047a614a6973bc789187b7fa30859e2
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-10-31 12:13:43 +0900

    subject_list 설정

--- all-clear/src/App.js
+++ all-clear/src/App.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
--- all-clear/src/routes/page1.jsx
+++ all-clear/src/routes/page1.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import SubjectList from "../components/SubjectList";
 import Detail from "../components/Detail";
+
 export default function Page1() {
 
   const DETAIL_SEARCH = "02. 세부검색";
     return (
-      <React.Fragment>
+      <>
         <div id="required-selection">required-selection</div>
         <Detail title={DETAIL_SEARCH}></Detail>
-        <div id="subjects-list">subjects-list</div>
-      </React.Fragment>
+        <SubjectList></SubjectList>
+      </>
     );
   }
