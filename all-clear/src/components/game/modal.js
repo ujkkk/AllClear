308a62fc987d885d80640516af2ad7c9f3f89bab
syy0O <86238720+syy0O@users.noreply.github.com>
2022-11-21 21:48:33 +0900

    게임 설명 modal css 수정

--- all-clear/src/components/game/modal.js
+++ all-clear/src/components/game/modal.js
@@ -10,11 +10,19 @@ function closeModal() {
   return (
     <div className="Modal" onClick={closeModal}>
       <div className="modalBody" onClick={(e) => e.stopPropagation()}>       
-                <span id="game-info-title">게임 설명</span>
+      <span id="game-info-title">게임 설명</span>
                 <button id="modalCloseBtn" onClick={closeModal}>
                 ✖
                 </button>  
-        {props.children}
+                <p id="game-slogan">누구보다 빠르게 <span style={{color:"#FBDF07"}}>"All-Clear" </span></p>
+                <div id="modal-division"></div>
+                <div id="game-info-contents">
+                <div class="game-info-btn">1</div>
+                <div class="game-info-btn">2</div>
+                <div class="game-info-btn">3</div>
+                </div>
+        {/* {props.children} */}
+       
       </div>
     </div>
   );
