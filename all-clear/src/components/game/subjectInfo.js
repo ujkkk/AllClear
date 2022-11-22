28577974ee6cd816d723cf4dc98f5a5e046dae19
daneng4 <daneng4@naver.com>
2022-11-22 11:53:31 +0900

    gamescreen-table

--- all-clear/src/components/game/subjectInfo.js
+++ all-clear/src/components/game/subjectInfo.js
@@ -8,37 +8,60 @@ const SubjectInfo=({subject})=>{
     var subjectData = subjectList.filter((data) => data.subject_id == subject.subject_id)
    return(
     <div style={{ width:"800px", height:"130px", border: "1px solid black", fontFamily : "Nanum-Gothic"}}>
-        <table>
+        <table style={{marginLeft : "20px", width : "800px"}} >
             <tbody>
                 <tr>
-                    <td style={{width : "390px"}}>
+                    <td style={{width : "400px"}}>
                         {subjectData[0].code} {subjectData[0].name}
                     </td>
                     
-                    <td style={{width : "25px"}}>
-                        {subject.type} |
-                    </td>
-                    <td style={{width : "45px"}}>
-                        {"전선"} |
-                    </td>
-                    <td style={{width : "55px"}}>
-                        {"3학점"} |
-                    </td>
-                    <td style={{width : "45px"}}>
-                        {"주간"} |
-                    </td>
-                    <td style={{width : "55px"}}> 
-                        {subjectData.map(elements => {return elements.grade})} |
-                    </td>
-                    <td style={{width : "60px"}}>
-                        {subjectData[0].professor.join(", ")}
+                    <td style={{width : "285px"}}>
+                    
+                        {subject.type} | {"전선"} | {"3학점"} | {"주간"} | {subjectData.map(elements => {return elements.grade})} | {subjectData[0].professor.join(", ")}
+                    
                     </td>
 
-                    <button style={{marginTop : "8px",marginLeft : "60px" ,backgroundColor : "#2AAA45",
+                    <button style={{marginTop : "8px",marginLeft : "30px" ,backgroundColor : "#2AAA45",
                      border : "none", height: "33px", color:"white", borderRadius:"5px"}}>
                         신청
                     </button>
 
+                </tr>
+            </tbody>
+        </table>
+        <table  border = "1" style={{marginLeft : "20px" ,float : "left", width : "300px", textAlign : "center"}}>
+            <tbody>
+                <tr>
+                    <td style={{width : "50px"}}> 타과1</td>
+                    <td style={{width : "50px"}}> 타과2</td>
+                    <td style={{width : "50px"}}> 타과3</td>
+                    <td style={{width : "50px"}}> 타과4</td>
+                    <td style={{width : "50px"}}> 편입</td>
+                    <td style={{width : "50px"}}> 자과</td>
+
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                                
+                </tr>
+            </tbody>
+        </table>
+        <table style={{float : "left", width : "95px"}}>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        <table style={{width : "155px",fontSize : "13px", color : "#1CC8E8"}}>
+            <tbody>
+                <tr>
+                    <td>수업 장소</td>
                 </tr>
             </tbody>
         </table>
