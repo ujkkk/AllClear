b7cde7cc2f972a6a21d92856f62fc8db78e72aad
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-11-22 13:55:54 +0900

    제발 ㅜㅜㅜㅜ

--- all-clear/src/components/game/applyClasses.js
+++ all-clear/src/components/game/applyClasses.js
@@ -1,27 +1,47 @@
 import React from "react";
 import SubjectInfo from "./subjectInfo"
 import ScheduleRow from "../preset/scheduleRow";
+import classDivisionData from "../../data/classDivision.json";
+import subjectsData from "../../data/subjectList.json";
+const ApplyClasses = ({ preset, selectedNum, addClass = f => f }) => {
 
 
-const ApplyClasses = ({preset, selectedNum}) => {
 
     //preset은 잘 받아 오는데 selectedNUm이 안된다.
-    console.log("selectedNum : "+  selectedNum)
+    console.log("selectedNum : " + selectedNum)
     var selectedPreset = []
-    if(selectedNum ==0)
-        selectedPreset= preset.preset1;
-    else if(selectedNum ==1)   
-        selectedPreset= preset.preset2;
-    else if(selectedNum ==2)   
-        selectedPreset= preset.preset2;
-    else 
-        selectedPreset= preset.preset1;
+    if (selectedNum == 0)
+        selectedPreset = preset.preset1;
+    else if (selectedNum == 1)
+        selectedPreset = preset.preset2;
+    else if (selectedNum == 2)
+        selectedPreset = preset.preset2;
+    else
+        selectedPreset = preset.preset1;
     console.log(selectedPreset)
     return (
-       <>
-       {selectedPreset.map((subject) => <SubjectInfo subject={subject} />)}
-       </>
-    
-)}
+        <>
+            {
+                selectedPreset.map((subject) => {
+                    return subjectsData.map((sub, idx) => {
+                        if (subject.subject_id === sub.subject_id) {
+                            return classDivisionData.map((classSub) => {
+                                if (classSub.subject_id === sub.subject_id) {
+                                    return classSub.class.map((data, i) => {
+                                        if (subject.type === data.type)
+                                            return <SubjectInfo key={String(idx) + String(i)} classData={data} subjectsData={sub} addClass={addClass} />
+                                    })
+                                }
+
+                            })
+                        }
+                    })
+                })
+            }
+            {/* {selectedPreset.map((subject) => <SubjectInfo subject={subject} addClass={addClass} />)} */}
+        </>
+
+    )
+}
 
 export default ApplyClasses;
